7819635708d0581000e6dd710745e6c76abd46dd
Wesley DuSell <dusellfam@gmail.com>
2025-07-02 22:35:29 -0400

    Added card and styles update to th emain list

--- src/app/routes/mfe-remote-list.component.ts
+++ src/app/routes/mfe-remote-list.component.ts
@@ -4,35 +4,41 @@ import { MfeRemoteComponent } from '../components/mfe-remote.component';
 import { lastValueFrom } from 'rxjs';
 import { IMfeRemote, MfeRemoteService } from '../services/mfe-remote.service';
 import { AsyncPipe } from '@angular/common';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'ngx-mfe-remotes',
-  imports: [HeroComponent, MfeRemoteComponent, AsyncPipe],
+  imports: [HeroComponent, MfeRemoteComponent, AsyncPipe, MatCard],
   template: `
     <ngx-hero></ngx-hero>
-    @for (mfeRemote of mfeRemotes | async; track $index) {
-    <ngx-mfe-remote
-      [initialValue]="mfeRemote"
-      (update)="updateMfeRemote($event)"
-      (archive)="archiveMfeRemote($event)"
-      (delete)="deleteMfeRemote($event)"
-    ></ngx-mfe-remote>
-    }
+    <mat-card appearance="outlined">
+      @for (mfeRemote of mfeRemotes | async; track $index) {
+      <ngx-mfe-remote
+        [initialValue]="mfeRemote"
+        (update)="updateMfeRemote($event)"
+        (archive)="archiveMfeRemote($event)"
+        (delete)="deleteMfeRemote($event)"
+      ></ngx-mfe-remote>
+      }
+    </mat-card>
   `,
   styles: [
     `
       :host {
         margin-top: 56px;
-        padding-bottom: 56px;
         display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
         gap: 3em;
-        
-        ngx-mfe-remote {
+
+        mat-card {
           width: 100%;
           max-width: 800px;
+          margin-bottom: 2em;
+          padding: 1em;
+          display: flex;
+          flex-direction: column;
+          gap: 1em;
         }
       }
     `,
