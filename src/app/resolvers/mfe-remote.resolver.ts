efd377c6744648a35db1349d6532675dffe2e248
Wesley DuSell <dusellfam@gmail.com>
2025-08-24 23:18:21 -0400

    Testing new shared types 2

--- src/app/resolvers/mfe-remote.resolver.ts
+++ src/app/resolvers/mfe-remote.resolver.ts
@@ -1,9 +1,11 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { Observable } from 'rxjs';
-import { IMfeRemote, MfeRemoteService } from '../services/mfe-remote.service';
+import { MfeRemoteService } from '../services/mfe-remote.service';
 
-type MfeRemoteResolver = ResolveFn<Observable<IMfeRemote[]>>;
+import type { MfeRemoteDto } from '@tmdjr/ngx-mfe-orchestrator-contracts';
+
+type MfeRemoteResolver = ResolveFn<Observable<MfeRemoteDto[]>>;
 export const mfeRemoteResolver: MfeRemoteResolver = () => {
   return inject(MfeRemoteService).fetchMfeRemotes();
 };
