abb4b84c5228b722c64206f9cacedaa4aefd12c8
Wesley DuSell <dusellfam@gmail.com>
2025-09-19 19:35:29 -0400

    Fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/mfe-orchestrator',
+    redirectTo: '/mfe-orchestrator/list-mfe-remotes',
     pathMatch: 'full',
   },
 ];
