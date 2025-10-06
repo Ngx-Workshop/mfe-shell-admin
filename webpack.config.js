645abfa0b117ef7cde0a74d6448d3fcdc1c0b7b7
Wesley DuSell <dusellfam@gmail.com>
2025-10-05 21:46:48 -0400

    Bump meta

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ module.exports = withModuleFederationPlugin({
     '@tmdjr/ngx-navigational-list': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: '0.0.9',
+      requiredVersion: '0.0.10',
     },
   },
 });
